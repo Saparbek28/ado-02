56016af1ca688f11bb7875a900a4a1ef2bdd9b12
Saparbek28 <46023456+Saparbek28@users.noreply.github.com>
2019-04-25 21:23:48 +0600

    first commit

---
+++ Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HomeWorkAdo02
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkAdo02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var group = new Group()
+            {
+                Name = "Sapar"
+            };
+
+            using (var context = new AppContext())
+            {
+                context.Group.Add(group);
+            }
+        }
+    }
+}
